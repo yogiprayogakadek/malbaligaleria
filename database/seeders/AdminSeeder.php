f4a11d32bd54e896e3d39a0b6cdff407acfc785a
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-01-20 17:48:57 +0800

    update admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,18 +13,59 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::firstOrCreate([
-            'tenant_id' => null,
-            'name' => 'Yogi Prayoga',
-            'email' => 'yogi@malbaligaleria.com',
-            'password' => 'mbg@2025_',
-            'phone' => '082237188923',
-            'avatar' => null,
-            'status' => 'approved',
-            'is_active' => true,
-            'email_verified_at' => '2025-11-28 02:48:51',
-        ]);
+        $users = [
+            [
+                'tenant_id' => null,
+                'name' => 'Yogi Prayoga',
+                'email' => 'yogi@malbaligaleria.com',
+                'password' => 'mbg@2025_',
+                'phone' => '082237188923',
+                'avatar' => null,
+                'status' => 'approved',
+                'is_active' => true,
+                'email_verified_at' => '2025-11-28 02:48:51',
+            ],
+            [
+                'tenant_id' => null,
+                'name' => 'Wahyu',
+                'email' => 'wahyu@malbaligaleria.com',
+                'password' => 'wahyu#mbg2025',
+                'phone' => '082237188923',
+                'avatar' => null,
+                'status' => 'approved',
+                'is_active' => true,
+                'email_verified_at' => '2025-11-28 02:48:51',
+            ],
+            [
+                'tenant_id' => null,
+                'name' => 'Riri',
+                'email' => 'riri@malbaligaleria.com',
+                'password' => 'riri#mbg2025',
+                'phone' => '082237188923',
+                'avatar' => null,
+                'status' => 'approved',
+                'is_active' => true,
+                'email_verified_at' => '2025-11-28 02:48:51',
+            ]
+        ];
 
-        $user->assignRole('admin');
+        foreach ($users as $user) {
+            $u = User::firstOrCreate($user);
+            $u->assignRole('admin');
+        }
+
+        // $user = User::firstOrCreate([
+        //     'tenant_id' => null,
+        //     'name' => 'Yogi Prayoga',
+        //     'email' => 'yogi@malbaligaleria.com',
+        //     'password' => 'mbg@2025_',
+        //     'phone' => '082237188923',
+        //     'avatar' => null,
+        //     'status' => 'approved',
+        //     'is_active' => true,
+        //     'email_verified_at' => '2025-11-28 02:48:51',
+        // ]);
+
+        // $user->assignRole('admin');
     }
 }
