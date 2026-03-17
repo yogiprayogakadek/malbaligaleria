712b16d111be0db429e6a77a1d1c9836ea691b0f
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-17 16:12:27 +0800

    Change promotion CTA text to Learn More

--- public/assets/frontend/js/promotion/promotion.js
+++ public/assets/frontend/js/promotion/promotion.js
@@ -500,7 +500,7 @@ async function displayPromotions() {
                 ${countdownBarHTML}
                 <p class="promotion-description">${promo.description}</p>
                 <button class="view-details-btn">
-                    View Details
+                    Learn More
                     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M5 12h14M12 5l7 7-7 7"/>
                     </svg>
