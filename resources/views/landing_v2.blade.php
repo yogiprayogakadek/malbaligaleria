80ba30263a1f6599529bbf1d14fcc1f90120bdfe
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-24 09:58:04 +0800

    update facade

--- resources/views/landing_v2.blade.php
+++ resources/views/landing_v2.blade.php
@@ -138,10 +138,10 @@
 
     <section class="hero" id="home">
         <div class="hero-slider" id="heroSlider">
-            <div class="hero-slide active" style="background-image: url('{{ asset('assets/bg_front.jfif') }}')">
+            <div class="hero-slide active" style="background-image: url('{{ asset('assets/facade/landscape.jpg') }}')">
             </div>
             <div class="hero-slide" style="background-image: url('{{ asset('assets/bg_front.jfif') }}')"></div>
-            <div class="hero-slide" style="background-image: url('{{ asset('assets/bg_front.jfif') }}')"></div>
+            <!-- <div class="hero-slide" style="background-image: url('{{ asset('assets/bg_front.jfif') }}')"></div> -->
             {{-- <div class="hero-slide active" style="background-image: url('{{ asset('assets/frontend/images/hero/hero_slide_1.png') }}')"></div>
             <div class="hero-slide" style="background-image: url('{{ asset('assets/frontend/images/hero/hero_slide_2.png') }}')"></div>
             <div class="hero-slide" style="background-image: url('{{ asset('assets/frontend/images/hero/hero_slide_3.png') }}')"></div> --}}
