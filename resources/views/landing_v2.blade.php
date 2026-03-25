1178e2061d7bffbfa9454fd9bad7b8aabc58f821
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-25 18:43:40 +0800

    update regular to this month event

--- resources/views/landing_v2.blade.php
+++ resources/views/landing_v2.blade.php
@@ -109,7 +109,7 @@
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="{{ route('frontend.directory.index') }}">Tenants Directory</a></li>
-                <li><a href="{{ route('frontend.landing') }}#events">Events</a></li>
+                <li><a href="{{ route('frontend.landing') }}#regular-shows">Events</a></li>
                 <li><a href="{{ route('frontend.promotion.index') }}">Promo</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
