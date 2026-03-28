6a9473e63d82816283c6896cb25295efdedc59a9
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-28 18:53:04 +0800

    update new store card

--- resources/views/frontend/new-store/index.blade.php
+++ resources/views/frontend/new-store/index.blade.php
@@ -463,7 +463,7 @@
 
     </div> {{-- End .main-content --}}
 
-    @include('frontend.landing_v2.footer')
+    @include('frontend.partials.footer_v2')
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
