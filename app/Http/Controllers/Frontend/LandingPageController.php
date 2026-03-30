6fb52e5a43bbb7a59446ad8725c78c5004ffb06c
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-30 14:15:55 +0800

    update highlights

--- app/Http/Controllers/Frontend/LandingPageController.php
+++ app/Http/Controllers/Frontend/LandingPageController.php
@@ -45,8 +45,7 @@ class LandingPageController extends Controller
         );
 
         $exhibitionEvents = $this->eventService->getExhibitionEvents(
-            ['*'],
-            // ['id', 'uuid', 'name', 'type', 'start_date', 'end_date', 'description', 'location', 'highlights'],
+            ['id', 'uuid', 'name', 'type', 'start_date', 'end_date', 'description', 'location', 'highlights'],
             [
                 'primaryPhoto:id,path,caption,event_id,is_primary'
             ]
