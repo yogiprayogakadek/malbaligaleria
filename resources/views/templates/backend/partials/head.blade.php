4ee7879554af955963e67bb1c32599da2c2682c3
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-31 00:06:45 +0800

    fix: add CSRF meta tag to backend head to fix 419 error on deletion

--- resources/views/templates/backend/partials/head.blade.php
+++ resources/views/templates/backend/partials/head.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
