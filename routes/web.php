a274075d31bb4094905167ee8041a6576d7368ad
yogiprayogakadek <yogiprayogakadek@gmail.com>
2026-03-31 09:37:51 +0800

    fix: add frontend route name prefix for landing pages

--- routes/web.php
+++ routes/web.php
@@ -42,15 +42,15 @@ Route::controller(LandingPageController::class)->name('frontend.')->group(functi
     Route::get('/event/{uuid}', 'getEventDetail')->name('event.detail');
 });
 
-Route::prefix('/directory')->name('directory.')->name('frontend.directory.')->group(function () {
+Route::prefix('/directory')->name('directory.')->group(function () {
     Route::get('/', [DirectoryController::class, 'index'])->name('index');
 });
 
-Route::prefix('/promotion')->name('promotion.')->name('frontend.promotion.')->group(function () {
+Route::prefix('/promotion')->name('promotion.')->group(function () {
     Route::get('/', [PromotionController::class, 'index'])->name('index');
 });
 
-Route::prefix('/new-store')->name('new-store.')->name('frontend.new-store.')->group(function () {
+Route::prefix('/new-store')->name('new-store.')->group(function () {
     Route::get('/', [NewStoreController::class, 'index'])->name('index');
 });
 
